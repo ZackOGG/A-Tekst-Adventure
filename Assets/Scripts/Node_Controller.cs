e25a7b1248405844961314ac49893062beb7b272
ZackOGGSurf <Orange_goo_games@outlook.com>
2018-07-06 03:16:57 +0100

    End of Day Upload 05_07_2018

--- Assets/Scripts/Node_Controller.cs
+++ Assets/Scripts/Node_Controller.cs
@@ -1,17 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.Assertions;
 using UnityEngine;
 
 public class Node_Controller : MonoBehaviour {
     
     [Header("Direction Locations")]
     [SerializeField] string thisLocationText;
-    [SerializeField] OptionContents optionOneNode;
-    [SerializeField] OptionContents optionTwoNode;
-    [SerializeField] OptionContents optionThreeNode;
-    [SerializeField] OptionContents optionFourNode;
-    [SerializeField] OptionContents optionFiveNode;
-    [SerializeField] OptionContents optionSixNode;
+    [SerializeField] OptionContents[] optionsList;
 
     [SerializeField] static string userInputOptionOneText = "Press the up arrow key to";
     [SerializeField] static string userInputOptionTwoText = "Press the right arrow key to";
@@ -26,18 +22,27 @@ public class Node_Controller : MonoBehaviour {
 
     private bool waitingForPlayer = true;
     private bool waitingToContinue = false;
+    private int[] optionOrder;
+    public OptionContents[] options = new OptionContents[6];
 
     // Use this for initialization
     void Start ()
     {
+        print(optionsList);
+        if (optionsList.Length != 6)
+        {
+            Debug.LogError("Error: Node has less than 6 options. Do not delete options if you don't want that many options leave it blank.");
+        }
         OpeningDescription();
         ReadOptions();
+       
 	}
-	
+
+
     private void OpeningDescription()
     {
         //Read the opening description
-        print(thisLocationText + " " + optionOneNode.nodeDescription + " " + optionTwoNode.nodeDescription + " " + optionThreeNode.nodeDescription + " " + optionFourNode.nodeDescription + " " + optionFiveNode.nodeDescription + " " + optionSixNode.nodeDescription + "What would you like to do?");
+        print(thisLocationText + " " + optionsList[0].nodeDescription + " " + optionsList[1].nodeDescription + " " + optionsList[2].nodeDescription + " " + optionsList[3].nodeDescription + " " + optionsList[4].nodeDescription + " " + optionsList[5].nodeDescription + "What would you like to do?");
     }
     private void ReadOptions()
     {
@@ -46,49 +51,56 @@ public class Node_Controller : MonoBehaviour {
         //Change button three to read option three + Text
         //change button four to read option four + Text 
 
-        print(userInputOptionOneText + " " + optionOneNode.optionText);
-        print(userInputOptionTwoText + " " + optionTwoNode.optionText);
-        print(userInputOptionThreeText + " " + optionThreeNode.optionText);
-        print(userInputOptionFourText + " " + optionFourNode.optionText);
-        print(userInputOptionFiveText + " " + optionFiveNode.optionText);
-        print(userInputOptionSixText + " " + optionSixNode.optionText);
+        print(userInputOptionOneText + " " + optionsList[0].optionText);
+        print(userInputOptionTwoText + " " + optionsList[1].optionText);
+        print(userInputOptionThreeText + " " + optionsList[2].optionText);
+        print(userInputOptionFourText + " " + optionsList[3].optionText);
+        print(userInputOptionFiveText + " " + optionsList[4].optionText);
+        print(userInputOptionSixText + " " + optionsList[5].optionText);
+
+        waitingForPlayer = true;
     }
 
     private void Update()
     {
         if(waitingForPlayer)
         {
-
+            WaitingForPlayer();
         }
        
     }
 
     private void WaitingForPlayer() // Waiting for the player to pick inital option
     {
-        if (Input.GetKeyDown(KeyCode.UpArrow))
+        if (Input.GetKeyDown(KeyCode.UpArrow) && !Input.GetKeyDown(KeyCode.DownArrow))
         {
-
+            AtemptActopn(options[0]);
         }
-        if (Input.GetKeyDown(KeyCode.RightArrow))
+        if (Input.GetKeyDown(KeyCode.RightArrow) && !Input.GetKeyDown(KeyCode.LeftArrow))
         {
-
+            AtemptActopn(options[1]);
         }
-        if (Input.GetKeyDown(KeyCode.DownArrow))
+        if (Input.GetKeyDown(KeyCode.DownArrow) && !Input.GetKeyDown(KeyCode.UpArrow))
         {
-
+            AtemptActopn(options[2]);
         }
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        if (Input.GetKeyDown(KeyCode.LeftArrow) && !Input.GetKeyDown(KeyCode.RightArrow))
         {
-
+            AtemptActopn(options[3]);
         }
         if(Input.GetKeyDown(KeyCode.UpArrow) && Input.GetKeyDown(KeyCode.DownArrow))
         {
-
+            AtemptActopn(options[4]);
         }
         if (Input.GetKeyDown(KeyCode.LeftArrow) && Input.GetKeyDown(KeyCode.RightArrow))
         {
-
+            AtemptActopn(options[5]);
         }
+    }
+
+    private void SetOptions()
+    {
+        //NEXT Create a foreach with the options list in that checks through them all for each one that is not blank and adds them to the options array.
     }
 
     private void AtemptActopn(OptionContents optionCon)
